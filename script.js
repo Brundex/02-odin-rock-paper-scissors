6460f30a2d5e1c32ee27962f5e4ca471bbda742b
Brundex <puodziunasb@gmail,com>
2022-06-18 02:07:31 -0300

    add comments + organize code

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+// call randomNumber function and use switch cases to assign each number to a game value
+function computerPlay(){
+    let computerChoice = randomNumber();
+
+    switch(computerChoice){
+        case 1:
+            computerChoice = "ROCK";
+            return computerChoice;
+        case 2:
+            computerChoice = "PAPER";
+            return computerChoice;
+        case 3:
+            computerChoice = "SCISSORS";
+            return computerChoice;
+    }
+}
+
+
+// simulate one round
+function playRound(playerSelection=promptUntilValid(), computerSelection=computerPlay()){
+    console.log(`You pick ${playerSelection}`);
+    console.log(`I pick ${computerSelection}`);
+    winner = declareWinner(playerSelection, computerSelection);
+}
+
+// play a game of 5 rounds. tied rounds are not counted
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let i = 0;
+    while(i !== 5){
+        playRound();
+        //only increment i if there is a winner
+        console.log(i);
+        if (winner === "player"){
+            playerScore++;
+            i++;
+        }
+        else if (winner === "computer"){
+            computerScore++;
+            i++;
+        }
+        
+        //if anyone reaches 3 points, game is over as winner is already declared
+        console.log(`You: ${playerScore}; Me: ${computerScore}`);
+        if (computerScore === 3){
+            console.log("");
+            console.log("You lose!");
+            break;
+        }
+        if (playerScore === 3){
+            console.log("");
+            console.log("You win!");
+            break;
+        }
+     }
+}
+
+// Here is the game
+game();
+
+
+
 //helper function to randomize between 1-3. 
 //Taken from https://www.educative.io/answers/how-to-generate-a-random-number-between-a-range-in-javascript
 function randomNumber(min=1, max=4){
@@ -12,59 +75,6 @@ function randomNumber(min=1, max=4){
     return rand;
 }
 
-//call randomNumber function and use switch cases to assign each value to a game value
-function computerPlay(){
-    let computerChoice = randomNumber();
-    switch(computerChoice){
-        case 1:
-            computerChoice = "ROCK";
-            return computerChoice;
-        case 2:
-            computerChoice = "PAPER";
-            return computerChoice;
-        case 3:
-            computerChoice = "SCISSORS";
-            return computerChoice;
-    }
-}
-
-function playRound(playerSelection=promptUntilValid(), computerSelection=computerPlay()){
-    console.log(playerSelection);
-    console.log(computerSelection);
-    winner = declareWinner(playerSelection, computerSelection);
-}
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let i = 0;
-    while(i !== 5){
-        playRound()
-        console.log(i);
-        if (winner === "player"){
-            playerScore++;
-            i++;
-        }
-        else if (winner === "computer"){
-            computerScore++;
-            i++;
-        }
-        console.log(`You: ${playerScore}; Me: ${computerScore}`);
-        if (computerScore === 3){
-            console.log("You lose!");
-            break;
-        }
-        if (playerScore === 3){
-            console.log("You win!");
-            break;
-        }
-     }
-}
-
-game();
-
-
-
 
 //helper function to get valid value
 function promptUntilValid(playerSelection){
@@ -78,7 +88,7 @@ function promptUntilValid(playerSelection){
     }
 }
 
-//helper function to figure out winner
+//helper function to figure out winner and address all possible outcomes
 function declareWinner(playerSelection, computerSelection){
     switch(playerSelection) {
         case "ROCK":
@@ -94,6 +104,7 @@ function declareWinner(playerSelection, computerSelection){
                 console.log("Ouch! Easy!");
                 return "player";
             }
+
         case "PAPER":
             if(computerSelection == "ROCK"){
                 console.log("Ouch! Easy!");
