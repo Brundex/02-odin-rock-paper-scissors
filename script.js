e8dcdb3db3b40c314d3c1e46781faaab2c027d0c
Brundex <puodziunasb@gmail,com>
2022-06-17 23:37:55 -0300

    buggy functions

--- script.js
+++ script.js
@@ -12,17 +12,92 @@ function randomNumber(min=1, max=4){
     return rand;
 }
 
+//call randomNumber function and use switch cases to assign each value to a game value
 function computerPlay(){
-    //call randomNumber function and use switch cases to assign each value to a game value
-    computerChoice = randomNumber();
+    let computerChoice = randomNumber();
     switch(computerChoice){
         case 1:
-            return "ROCK";
+            computerChoice = "ROCK";
+            return computerChoice;
         case 2:
-            return "PAPER";
+            computerChoice = "PAPER";
+            return computerChoice;
         case 3:
-            return "SCISSORS";
+            computerChoice = "SCISSORS";
+            return computerChoice;
+    }
+}
+let playerScore = 0;
+let computerScore = 0;
+function playRound(playerSelection, computerSelection){
+    promptUntilValid(playerSelection);
+    computerSelection = computerPlay();
+    console.log(playerSelection);
+    console.log(computerSelection);
+    declareWinner(playerSelection, computerSelection);
+}
+
+playRound(promptUntilValid(), "PAPER");
+
+//helper function
+function promptUntilValid(playerSelection){
+    invalidValue = true;
+    while(invalidValue){
+        playerSelection = prompt("Rock, Paper, Scissors?").toUpperCase();
+        if (playerSelection === "ROCK" || playerSelection === "PAPER" || playerSelection === "SCISSORS"){
+            return playerSelection;
+        }
     }
 }
 
-console.log(randomNumber());
+//helper function
+function declareWinner(playerSelection, computerSelection){
+    switch(playerSelection) {
+        case "ROCK":
+            if(computerSelection == "ROCK"){
+                console.log("It's a tie!");
+                break;
+            }
+            else if(computerSelection == "PAPER"){
+                console.log("Ha! Got ya!");
+                computerScore++;
+                break;
+            }
+            else{
+                console.log("Ouch! Easy!");
+                playerScore++;
+                break;
+            }
+        case "PAPER":
+            if(computerSelection == "ROCK"){
+                console.log("Ouch! Easy!");
+                playerScore++;
+                break;
+            }
+            else if(computerSelection == "PAPER"){
+                console.log("It's a tie!");     
+                break;           
+            }
+            else{
+                console.log("Ha! Got ya!");
+                computerScore++;
+                break;
+            }
+
+        case "SCISSORS":
+            if(computerSelection == "ROCK"){
+                console.log("Ha! Got ya!");
+                computerScore++;
+                break;
+            }
+            else if(computerSelection == "PAPER"){
+                console.log("Ouch! Easy!");
+                playerScore++;
+                break;
+            }
+            else{
+                console.log("It's a tie!");
+                break;
+            }
+    }
+}
